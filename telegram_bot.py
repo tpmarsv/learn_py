0f5e640dcd9d2eda044bd7d290b7eca73bd43064
Mars <mars.v.mail@gmail.com>
2019-06-18 23:44:14 +0300

    Add date of today for req, sort and send 3 first

--- telegram_bot.py
+++ telegram_bot.py
@@ -4,6 +4,7 @@ import logging
 import json
 import requests
 import emoji
+from datetime import datetime
 
 # Настройки прокси
 PROXY = {'proxy_url': 'socks5://t1.learn.python.ru:1080', 'urllib3_proxy_kwargs': {'username': 'learn', 'password': 'python'}}
@@ -51,13 +52,23 @@ def bestrooms_byCity(bot, update):
     search_city = update.message.text
     update.message.reply_text("Ищем в " + search_city + " ..")
 
-    request_str = "http://engine.hotellook.com/api/v2/cache.json?location="+search_city+"&currency=rub&checkIn=2019-07-10&checkOut=2019-07-12&limit=4"
+    today_date = datetime.now()
+    today_date = today_date.strftime('%Y-%m-%d')
+
+    request_str = "http://engine.hotellook.com/api/v2/cache.json?location="+search_city+"&currency=rub&checkIn=" + today_date + "&checkOut=" + today_date + "&limit=10"
     response_api = requests.get(request_str)
     hotels_j = json.loads(response_api.text)
-    
-    message_name = hotels_j[0]['hotelName']
-    message_low_price = hotels_j[0]['priceFrom']
-    update.message.reply_text("Рекомендуем " + message_name + " по цене " + str(message_low_price) + " руб")
+    hotels_j_s = sort_list_of_dict(hotels_j, 'priceFrom')
+
+    for i in range(0, 3):
+        message_name = hotels_j_s[i]['hotelName']
+        message_low_price = hotels_j_s[i]['priceFrom']
+        update.message.reply_text("Рекомендуем " + message_name + " по цене " + str(message_low_price) + " руб")
+
+
+def sort_list_of_dict(ld_for_sort, sort_by_item):
+    sort_l = sorted(ld_for_sort, key=lambda x: x[sort_by_item])
+    return sort_l
 
 
 if __name__ == "__main__":
