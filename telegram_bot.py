8b4fa8b7b1ff617873ca297f594e5ca8a9d34b7e
Mars <mars.v.mail@gmail.com>
2019-06-29 11:29:43 +0300

    Add inline buttons and callback handler

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,4 +1,5 @@
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardMarkup
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, RegexHandler, ConversationHandler, CallbackQueryHandler
 import t_info as ti
 import logging
 import json
@@ -30,6 +31,7 @@ def main():
     dpatch = getroom_bot.dispatcher
     dpatch.add_handler(CommandHandler("start", greet_user))
     dpatch.add_handler(MessageHandler(Filters.text, bestrooms_byCity))
+    dpatch.add_handler(CallbackQueryHandler(button_cl))
 
     getroom_bot.start_polling()
     getroom_bot.idle()
@@ -60,15 +62,30 @@ def bestrooms_byCity(bot, update):
     hotels_j = json.loads(response_api.text)
     hotels_j_s = sort_list_of_dict(hotels_j, 'priceFrom')
 
+
     for i in range(0, 3):
-        message_name = hotels_j_s[i]['hotelName']
+        hotel_message_name = hotels_j_s[i]['hotelName']
         message_low_price = hotels_j_s[i]['priceFrom']
-        update.message.reply_text("Рекомендуем " + message_name + " по цене " + str(message_low_price) + " руб")
+        hotel_id = str(hotels_j_s[i]['hotelId'])
+        keyboard = [[InlineKeyboardButton('Бронь', callback_data='book in '+ hotel_id), InlineKeyboardButton('Детали', callback_data='details for ' + hotel_id)]]
+        update.message.reply_text("Рекомендуем " + hotel_message_name + " по цене " + str(message_low_price) + " руб")
+        update.message.reply_text("Нравится?", reply_markup=InlineKeyboardMarkup(keyboard, one_time_keyboard=True))
 
 
 def sort_list_of_dict(ld_for_sort, sort_by_item):
     sort_l = sorted(ld_for_sort, key=lambda x: x[sort_by_item])
     return sort_l
+
+
+def button_cl(bot, update):
+    '''
+    Inline button choose return actions: book room or details about rooms
+    '''
+    query = update.callback_query
+    if ('book' in query.data):
+        query.edit_message_text(text="Переходим к бронированию " + query.data)
+    elif ('details' in query.data):
+        query.edit_message_text(text="Переходим к номерам отеля " + query.data)
 
 
 if __name__ == "__main__":
